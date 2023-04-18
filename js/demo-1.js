8176a35cf24168420e0fe337e1e31f6f21072a30
EhabElshimi <ehab.developer.email@gmail.com>
2023-04-18 10:10:02 +0200

    first commit

--- js/demo-1.js
+++ js/demo-1.js
@@ -9,8 +9,8 @@
 
     function initHeader() {
         width = window.innerWidth;
-        height = window.innerHeight;
         target = {x: width/2, y: height/2};
+        height = 280;
 
         largeHeader = document.getElementById('large-header');
         largeHeader.style.height = height+'px';
